0b3df7653eb4073916b96f32ffaae12b22691e4e
Acid Bubbles <acidbubbles.anon@gmail.com>
2019-06-26 16:54:14 -0400

    Publish to a .zip for easier downloading

--- Publish.ps1
+++ Publish.ps1
@@ -2,14 +2,19 @@ Param (
     [Parameter(Mandatory = $true)][string]$Version
 )
 
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
 New-Item -ItemType Directory -Force -Path ./publish | Out-Null
 New-Item -ItemType Directory -Force -Path ./publish/src | Out-Null
+Remove-Item ImprovedPoV-$Version.zip -Force | Out-Null
 
 $Code = Get-Content -Path .\ADD_THIS.cslist -Raw
 $Code = $Code -Replace "src/Diagnostics.cs\r\n", ""
 $Code.Trim() | Set-Content -Path publish/ADD_THIS.cslist
 
 Get-Content ADD_THIS.cslist | ForEach-Object {
+    Write-Host Processing $_
     $Code = Get-Content -Path $_ -Raw
     $Code = $Code -Replace "#define POV_DIAGNOSTICS\r\n", ""
     $Code = $Code -Replace "(?ms)^\s*#if ?\(!?POV_DIAGNOSTICS\).+?#endif", ""
@@ -18,4 +23,6 @@ Get-Content ADD_THIS.cslist | ForEach-Object {
     $Code | Set-Content -Path ./publish/$_
 }
 
-Write-Host "Release available at ./publish/"
+7z a ImprovedPoV-$Version.zip ./publish/*
+
+Write-Host "Release available at ./publish/ and package at ./ImprovedPoV-$Version.zip"
