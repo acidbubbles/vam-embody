2e95ed63775f0910a60eaf287d762d9ed022ae67
Acid Bubbles <acidbubbles.anon@gmail.com>
2020-01-29 13:27:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Virt-A-Mate Wrist
+
+Better VR controller alignement in possession mode
+
+## License
+
+[MIT](LICENSE.md)
---
+++ Wrist.cs
@@ -0,0 +1,9 @@
+/// <summary>
+/// Wrist
+/// By Acidbubbles
+/// Better controller alignement when possessing
+/// Source: https://github.com/acidbubbles/vam-wrist
+/// </summary>
+public class Wrist : MVRScript
+{
+}
