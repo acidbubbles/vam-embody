460a68c7d1917b6abdf57f447ef0ab6f0554068d
Acid Bubbles <acidbubbles.anon@gmail.com>
2021-02-24 00:23:15 -0500

    Simplify height measure code to avoid weird null error

--- src/WorldScale/PlayerMeasurements.cs
+++ src/WorldScale/PlayerMeasurements.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using UnityEngine;
 
 public class PlayerMeasurements
 {
@@ -12,13 +13,13 @@ public class PlayerMeasurements
     public float MeasureHeight()
     {
         var headMotionControl = _context.trackers.motionControls.First(mc => mc.name == MotionControlNames.Head);
-        var lowestMotionControl = _context.trackers.motionControls
-            .Where(mc => mc.name == MotionControlNames.LeftHand || mc.name == MotionControlNames.RightHand)
-            .Where(t => t.enabled && t.SyncMotionControl())
-            .Min(vt => vt.currentMotionControl.position.y);
+        if (_context.leftHand != null && _context.rightHand != null)
+        {
+            var lowestMotionControl = Mathf.Min(_context.leftHand.position.y, _context.rightHand.position.y);
 
-        if (lowestMotionControl < headMotionControl.currentMotionControl.position.y / 10f)
-            return headMotionControl.currentMotionControl.position.y - lowestMotionControl;
+            if (lowestMotionControl < headMotionControl.currentMotionControl.position.y / 10f)
+                return headMotionControl.currentMotionControl.position.y - lowestMotionControl;
+        }
 
         return SuperController.singleton.heightAdjustTransform.InverseTransformPoint(headMotionControl.currentMotionControl.position).y;
     }
