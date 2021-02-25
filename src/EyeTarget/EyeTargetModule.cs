cdf2204490e991565965126e5122c01ec90c3846
Acid Bubbles <acidbubbles.anon@gmail.com>
2021-02-24 20:42:03 -0500

    Track mirrors and objects (unfinished)

--- src/EyeTarget/EyeTargetModule.cs
+++ src/EyeTarget/EyeTargetModule.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public interface IEyeTargetModule : IEmbodyModule
 {
+    JSONStorableBool trackMirrorsJSON { get; }
+    JSONStorableBool trackObjectsJSON { get; }
     void Rescan();
 }
 
@@ -22,15 +24,18 @@ public class EyeTargetModule : EmbodyModuleBase, IEyeTargetModule
     public override string label => Label;
     protected override bool shouldBeSelectedByDefault => true;
 
+    public JSONStorableBool trackMirrorsJSON { get; private set; }
+    public JSONStorableBool trackObjectsJSON { get; private set; }
+
     private EyesControl _eyeBehavior;
     private Transform _head;
     private Transform _lEye;
     private Transform _rEye;
     private FreeControllerV3 _eyeTarget;
-    private List<BoxCollider> _mirrors;
+    private readonly List<BoxCollider> _mirrors = new List<BoxCollider>();
+    private readonly List<Transform> _objects = new List<Transform>();
     private Vector3 _eyeTargetRestorePosition;
     private EyesControl.LookMode _eyeBehaviorRestoreLookMode;
-    private Transform _windowCamera;
 
     public override void Awake()
     {
@@ -42,19 +47,23 @@ public class EyeTargetModule : EmbodyModuleBase, IEyeTargetModule
         _lEye = eyes.First(eye => eye.name == "lEye").transform;
         _rEye = eyes.First(eye => eye.name == "rEye").transform;
         _eyeTarget = containingAtom.freeControllers.First(fc => fc.name == "eyeTargetControl");
+        trackMirrorsJSON = new JSONStorableBool("TrackMirrors", true, (bool _) => { if(enabled) Rescan(); });
+        trackObjectsJSON = new JSONStorableBool("TrackObjects", true, (bool _) => { if(enabled) Rescan(); });
     }
 
     public override bool BeforeEnable()
     {
         Rescan();
 
-        return _mirrors.Count > 0 || !ReferenceEquals(_windowCamera, null);
+        SuperController.LogMessage($"{_objects.Count}");
+
+        return _mirrors.Count > 0 || _objects.Count > 0;
     }
 
     public void Rescan()
     {
-        _mirrors = GetMirrors();
-        _windowCamera = GetWindowCamera();
+        UpdateMirrors();
+        UpdateObjects();
     }
 
     public override void OnEnable()
@@ -65,46 +74,92 @@ public class EyeTargetModule : EmbodyModuleBase, IEyeTargetModule
         _eyeBehaviorRestoreLookMode = _eyeBehavior.currentLookMode;
 
         _eyeBehavior.currentLookMode = EyesControl.LookMode.Target;
+
+        SuperController.singleton.onAtomUIDsChangedHandlers += ONAtomUIDsChanged;
     }
 
-    private static List<BoxCollider> GetMirrors()
+    private void UpdateMirrors()
     {
-        return SuperController.singleton.GetAtoms()
+        _mirrors.Clear();
+
+        if (!trackMirrorsJSON.val) return;
+
+        _mirrors.AddRange(SuperController.singleton.GetAtoms()
             .Where(a => _mirrorAtomTypes.Contains(a.type))
             .Where(a => a.on)
             .Select(a => a.GetComponentInChildren<BoxCollider>())
-            .Where(c => c != null)
-            .ToList();
+            .Where(c => c != null));
     }
 
-    private static Transform GetWindowCamera()
+    private void UpdateObjects()
     {
-        var atom = SuperController.singleton.GetAtoms()
-            .FirstOrDefault(a => a.type == "WindowCamera");
-        if (atom == null) return null;
-        if (atom.GetStorableByID("CameraControl")?.GetBoolParamValue("cameraOn") != true) return null;
-        return atom.mainController.control;
+        _objects.Clear();
+
+        if (!trackObjectsJSON.val) return;
+
+        foreach (var atom in SuperController.singleton.GetAtoms())
+        {
+            if (!atom.on) continue;
+
+            switch (atom.type)
+            {
+                case "WindowCamera":
+                {
+                    if (atom.GetStorableByID("CameraControl")?.GetBoolParamValue("cameraOn") != true) continue;
+                    _objects.Add(atom.mainController.control);
+                    break;
+                }
+                case "Person":
+                {
+                    foreach (var controller in atom.freeControllers.Where(fc => fc.name.EndsWith("Control")))
+                    {
+                        _objects.Add(controller.control);
+                    }
+                    break;
+                }
+            }
+        }
     }
 
     public override void OnDisable()
     {
         base.OnDisable();
 
+        SuperController.singleton.onAtomUIDsChangedHandlers -= ONAtomUIDsChanged;
+
          _eyeTarget.control.position = _eyeTargetRestorePosition;
          if(_eyeBehavior.currentLookMode != EyesControl.LookMode.Target)
              _eyeBehavior.currentLookMode = _eyeBehaviorRestoreLookMode;
 
          _mirrors.Clear();
-         _windowCamera = null;
+         _objects.Clear();
     }
 
     public void Update()
     {
-        if (_mirrors.Count == 0 && !ReferenceEquals(_windowCamera, null))
+        if (_objects.Count > 0)
         {
-            // TODO: This doesn't seem to look where you'd want, validate the position (should it use a child control?)
-            _eyeTarget.control.position = _windowCamera.position;
-            return;
+            var lastDistance = float.PositiveInfinity;
+            var lastPosition = Vector3.zero;
+            var planes = GeometryUtility.CalculateFrustumPlanes(SuperController.singleton.centerCameraTarget.targetCamera);
+            var cameraPosition = SuperController.singleton.centerCameraTarget.transform.position;
+
+            foreach (var o in _objects)
+            {
+                var position = o.position;
+                var bounds = new Bounds(position, new Vector3(0.25f, 0.25f, 0.25f));
+                if (!GeometryUtility.TestPlanesAABB(planes, bounds)) continue;
+                var distance = Vector3.Distance(bounds.center, cameraPosition);
+                if (distance > lastDistance) continue;
+                lastDistance = distance;
+                lastPosition = position;
+            }
+
+            if (!float.IsPositiveInfinity(lastDistance))
+            {
+                _eyeTarget.control.position = lastPosition;
+                return;
+            }
         }
 
         var eyesCenter = (_lEye.position + _rEye.position) / 2f;
@@ -152,5 +207,10 @@ public class EyeTargetModule : EmbodyModuleBase, IEyeTargetModule
         var planePoint = plane.ClosestPointOnPlane(eyesCenter);
         var reflectPosition = planePoint - (eyesCenter - planePoint);
         _eyeTarget.control.position = reflectPosition;
+    }
+
+    private void ONAtomUIDsChanged(List<string> uids)
+    {
+        Rescan();
     }
 }
--- src/EyeTarget/EyeTargetSettingsScreen.cs
+++ src/EyeTarget/EyeTargetSettingsScreen.cs
@@ -14,5 +14,8 @@
         if (ShowNotSelected(_eyeTarget.selectedJSON.val)) return;
 
         CreateText(new JSONStorableString("", "Moves the eye target so you will be looking back when looking at mirrors or the WindowCamera."), true);
+
+        CreateToggle(_eyeTarget.trackMirrorsJSON, true).label = "Track Mirrors";
+        CreateToggle(_eyeTarget.trackObjectsJSON, true).label = "Track Objects (Controls)";
     }
 }
